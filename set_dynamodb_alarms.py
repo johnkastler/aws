a2ba4d9ef49a9a00f906bfd0af491c043c1974a9
John Kastler <johnkastler@users.noreply.github.com>
2014-06-25 23:59:00 -0400

    added ability to define the Operations being used

--- set_dynamodb_alarms.py
+++ set_dynamodb_alarms.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
 
 from keyring import get_password
-from boto.sns import connect_to_region as connect_to_sns_region
 from boto.ec2.cloudwatch import connect_to_region as connect_to_cw_region
+from boto.ec2.cloudwatch import MetricAlarm
 from boto.dynamodb import connect_to_region as connect_to_db_region
+from time import sleep
 import lib.LoadBotoConfig as BotoConfig
 
-env = "qa"
 region = 'us-east-1'
+check = 'ThrottledRequests'
+operations = ['PutItem', 'Query', 'GetItem', 'BatchGetItem']
+topics = {
+              'demo' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX',
+              'qa' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX',
+              'prod' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX'
+            }
 
-id =  BotoConfig.config.get(env, 'aws_access_key_id')
-key = get_password(BotoConfig.config.get(env, 'keyring'), id)
+for env in topics:
+  topic = topics[env]
 
-db = connect_to_db_region(region, aws_access_key_id=id, aws_secret_access_key=key)
-sns = connect_to_sns_region(region, aws_access_key_id=id, aws_secret_access_key=key)
-cw = connect_to_cw_region(region, aws_access_key_id=id, aws_secret_access_key=key)
+  id = BotoConfig.config.get(env, 'aws_access_key_id')
+  key = get_password(BotoConfig.config.get(env, 'keyring'), id)
 
-topics = sns.get_all_topics()
-topic = topics[u'ListTopicsResponse']['ListTopicsResult']['Topics'][0]['TopicArn']
+  db = connect_to_db_region(region, aws_access_key_id=id, aws_secret_access_key=key)
+  cw = connect_to_cw_region(region, aws_access_key_id=id, aws_secret_access_key=key)
 
-tables = db.list_tables()
+  tables = db.list_tables()
 
-for table in tables:
-  counter = 0
-  metric_length = len(cw.list_metrics(dimensions={'TableName':table}, metric_name="ThrottledRequests"))
-  while counter <= metric_length:
-    try:
-      metrics = cw.list_metrics(dimensions={'TableName':table}, metric_name="ThrottledRequests")[counter]
-      alarm_name = 'ThrottledRequests_' + str(counter) + '_' + table
-      metrics.create_alarm(name=alarm_name, comparison='>=',
-                        threshold=1,
-                        period=300,
-                        evaluation_periods=2,
-                        statistic='Average',
-                        alarm_actions=[topic])
-      print("applied alarm to {0}".format(table))
-    except Exception:
-      pass
-    counter = counter + 1
+  for table in tables:
+      for operation in operations:
+          alarm_name = table + '-ThrottledRequests-' + operation
+          try:
+              alarm = MetricAlarm(name=alarm_name,
+                                  comparison='>=',
+                                  threshold=3,
+                                  period=300,
+                                  evaluation_periods=1,
+                                  statistic='Sum',
+                                  alarm_actions=[topic],
+                                  namespace='AWS/DynamoDB',
+                                  metric=check,
+                                  dimensions={"Operation":operation, "TableName":table})
+              cw.create_alarm(alarm)
+              print("Created {0} in {2} {1}".format(alarm_name, env.upper(), region))
+          except Exception:
+              print("COULD NOT CREATE {0}".format(alarm_name))
+          sleep(0.15)
