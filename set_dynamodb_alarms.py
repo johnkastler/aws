883ed46db2577c181bd525a57e447310c28e1ac3
John Kastler <johnkastler@users.noreply.github.com>
2014-06-26 01:27:52 -0400

    reworked how the Topic name is generated

--- set_dynamodb_alarms.py
+++ set_dynamodb_alarms.py
@@ -7,18 +7,17 @@ from boto.dynamodb import connect_to_region as connect_to_db_region
 from time import sleep
 import lib.LoadBotoConfig as BotoConfig
 
+app = 'XXXXXXXXXX'
+envs = ['dev', 'qa', 'staging', 'demo', 'prod']
 region = 'us-east-1'
 check = 'ThrottledRequests'
 operations = ['PutItem', 'Query', 'GetItem', 'BatchGetItem']
-topics = {
-              'demo' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX',
-              'qa' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX',
-              'prod' : 'arn:aws:sns:us-east-1:NNNNNNNNNNNNNNNN:XXXXXXXXXXXXXXXXX'
-            }
+topic = 'dynamodb'
 
-for env in topics:
-  topic = topics[env]
-
+for env in envs:
+  accountid = BotoConfig.config.get(env, 'accountid')
+  topic_name = 'arn:aws:sns:' + region + ':' + accountid + ':' + app + '-' + env + '-' + topic
+  
   id = BotoConfig.config.get(env, 'aws_access_key_id')
   key = get_password(BotoConfig.config.get(env, 'keyring'), id)
 
@@ -37,7 +36,7 @@ for env in topics:
                                   period=300,
                                   evaluation_periods=1,
                                   statistic='Sum',
-                                  alarm_actions=[topic],
+                                  alarm_actions=[topic_name],
                                   namespace='AWS/DynamoDB',
                                   metric=check,
                                   dimensions={"Operation":operation, "TableName":table})
@@ -45,4 +44,4 @@ for env in topics:
               print("Created {0} in {2} {1}".format(alarm_name, env.upper(), region))
           except Exception:
               print("COULD NOT CREATE {0}".format(alarm_name))
-          sleep(0.15)
+          sleep(0.25)
